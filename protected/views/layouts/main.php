a56f59e69696f76fd2778f8a96a2cbd29dfeddbf
iKarp <i.a.karpov@gmail.com>
2013-07-16 22:00:39 +0400

    Small changes

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -14,7 +14,6 @@
 
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-    <!--<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css" />-->
     <?php Yii::app()->bootstrap->register(); ?>
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
